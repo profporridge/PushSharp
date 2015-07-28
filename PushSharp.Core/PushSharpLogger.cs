6776cf7efdbd4392c0e7abd21ce6a51b8288b114
Chris Dryden <cdryden@pof.com>
2015-07-27 18:07:25 -0700

    added logger

---
+++ PushSharp.Core/PushSharpLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using PushSharp.Core;
+
+namespace GlobalPushService
+{
+    class PushSharpLogger : ILogger
+    {
+        public PushSharpLogger()
+        {
+        }
+
+        public void Debug(string format, params object[] objs)
+        {
+            Trace.WriteLine(String.Format(format,objs));
+        }
+
+        public void Info(string format, params object[] objs)
+        {
+            Trace.WriteLine(String.Format(format, objs));
+        }
+
+        public void Warning(string format, params object[] objs)
+        {
+            Trace.WriteLine(String.Format(format, objs));
+        }
+
+        public void Error(string format, params object[] objs)
+        {
+            Trace.WriteLine(String.Format(format, objs));
+            Trace.TraceError(format, objs);
+        }
+    }
+}
